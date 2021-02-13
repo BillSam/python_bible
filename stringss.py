a99c53d5d5f1bc78558a900be9946b4e77bce181
mbugua <email@email.com>
2021-02-13 16:40:11 +0300

    added the strings

---
+++ stringss.py
@@ -0,0 +1,17 @@
+name = "hello"
+print(type(name))
+
+message = 'John said to me /?" I will see you later "'
+
+romeo = '''
+API may stand for Application Programming Interface, but humans have to be able to read the APIs, too; someone has to do
+ the programming. Django REST Framework supports generating human-friendly HTML output for each resource when the HTML 
+ format is requested. These pages allow for easy browsing of resources, as well as forms for submitting data to the 
+ resources using POST, PUT, and DELETE.
+        '''
+print(message)
+print(romeo)
+
+hello = "Hello World!"
+print(hello)
+
